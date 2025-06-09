2a95aabe71442f481f5c89f90f5492650153d147
Ku1ikoff <kulikofaa@yandex.ru>
2025-06-09 17:51:08 +0500

    test

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 # Здесь можно тестировать функционал и писать мусорный код
+
+def calculator(func):
+    return func
+
+
+def plus(*args):
+    count = 0
+    for i in args:
+        count += i
+    print(count)
+
+
+calculator(plus(1, 2, 3, 4))
