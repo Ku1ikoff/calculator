cf818e0aaec6a286c119df584a96273dc31c3f7a
VitTalVik <vtalko@yandex.ru>
2025-06-10 21:56:51 +0500

    Создать новую ветку для тестов

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# Начнем путь к мечте!
-# Вперед и только вперед!!!
+print("Добро пожаловать в калькулятор")
+
+number_1 = int(input("Введите первое число: "))
+number_2 = int(input("Введите второе число: "))
+
+user_operation = input("Введите знак операции: ")
+
+operation = {
+    "+": lambda num_1, num_2: num_1 + num_2,
+}
+
+result = operation[user_operation](number_1, number_2)
+print(result)
